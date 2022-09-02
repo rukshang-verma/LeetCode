ca1198c3b416f2421e9b8c6ca5ee3ff5defe0528
rukshang-verma <69439438+rukshang-verma@users.noreply.github.com>
2022-09-02 02:04:18 -0400

    Time: 823 ms (24.30%), Space: 26.2 MB (29.03%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,22 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        # dict1 = {}
+        # count = 0
+        # for x in nums: 
+        #     if x in dict1:
+        #         dict1[x] = dict1[x] + 1
+        #     else: 
+        #         dict[x] == 1
+        # if dict[x].value >= 2:
+        #     return True
+        # return False
+        
+        nums.sort()
+        
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
+        
+        
