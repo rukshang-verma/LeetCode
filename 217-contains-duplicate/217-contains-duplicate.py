e276bb76d130719b0c88456e3af111f670896ff4
rukshang-verma <69439438+rukshang-verma@users.noreply.github.com>
2022-09-02 13:33:08 -0400

    Time: 889 ms (13.97%), Space: 26.2 MB (29.03%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -12,11 +12,13 @@ class Solution:
         #     return True
         # return False
         
-        nums.sort()
+#         nums.sort()
         
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
-                return True
-        return False
+#         for i in range(len(nums)-1):
+#             if nums[i] == nums[i+1]:
+#                 return True
+#         return False
+
+        return len(nums) != len(set(nums))
         
         
